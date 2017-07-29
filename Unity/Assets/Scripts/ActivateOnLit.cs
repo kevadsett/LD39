46c4b823ef2cfe8e9375d4fedbf1cc17ebfa15e2
Kev Adsett <kev.adsett@gmail.com>
2017-07-29 18:20:12 +0100

    Light cone and enemy ball.

---
+++ Unity/Assets/Scripts/ActivateOnLit.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class ActivateOnLit : MonoBehaviour {
+	public float ArcAngle = Mathf.PI / 4;
+
+	public float Radius = 10f;
+
+	public Transform Player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+
+		Vector3 rads = Mathf.Deg2Rad * Player.eulerAngles;
+
+		Vector3 ArcEndPoint = (Radius * new Vector3 (Mathf.Cos (-rads.y), 0, Mathf.Sin (-rads.y)));
+		Gizmos.DrawLine(Player.position, Player.position + ArcEndPoint);
+	}
+}
