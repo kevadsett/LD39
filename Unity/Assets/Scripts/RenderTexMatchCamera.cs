2440eaf396a2961407ae02fd9ef8a2c81c6013f5
gav <notoriousknave@gmail.com>
2017-07-29 21:49:36 +0100

    light buffer stuffer

---
+++ Unity/Assets/Scripts/RenderTexMatchCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTexMatchCamera : MonoBehaviour {
+	Camera cam;
+	RenderTexture rt;
+
+	void Awake () {
+		cam = GetComponent<Camera> ();
+	}
+
+	void Update ()  {
+		if (rt == null || rt.width != Screen.width || rt.height != Screen.height) {
+			rt = new RenderTexture (Screen.width, Screen.height, 16, RenderTextureFormat.ARGB32);
+			cam.targetTexture = rt;
+			Shader.SetGlobalTexture ("_LightBuffer", rt);
+		}
+	}
+}
