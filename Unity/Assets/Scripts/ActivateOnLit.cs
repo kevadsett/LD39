6e8d8327e5b0236a333ccc4818d1e9d8effacb84
Kev Adsett <kev.adsett@gmail.com>
2017-07-29 18:30:37 +0100

    Get the angles right, mate.

--- Unity/Assets/Scripts/ActivateOnLit.cs
+++ Unity/Assets/Scripts/ActivateOnLit.cs
@@ -22,9 +22,12 @@ public class ActivateOnLit : MonoBehaviour {
 	{
 		Gizmos.color = Color.green;
 
-		Vector3 rads = Mathf.Deg2Rad * Player.eulerAngles;
+		Vector3 rads = Mathf.Deg2Rad * (Player.eulerAngles - Vector3.up * 90);
 
-		Vector3 ArcEndPoint = (Radius * new Vector3 (Mathf.Cos (-rads.y), 0, Mathf.Sin (-rads.y)));
-		Gizmos.DrawLine(Player.position, Player.position + ArcEndPoint);
+		Vector3 ArcEndPoint1 = (Radius * new Vector3 (Mathf.Cos ((ArcAngle / 2) -rads.y), 0, Mathf.Sin ((ArcAngle / 2) - rads.y)));
+		Gizmos.DrawLine(Player.position, Player.position + ArcEndPoint1);
+
+		Vector3 ArcEndPoint2 = (Radius * new Vector3 (Mathf.Cos (-rads.y - (ArcAngle / 2)), 0, Mathf.Sin (-rads.y - (ArcAngle / 2))));
+		Gizmos.DrawLine(Player.position, Player.position + ArcEndPoint2);
 	}
 }
