dac5f71ae1efa9d73976bc110f38dfa68280831b
Kev Adsett <kev.adsett@gmail.com>
2017-07-30 22:16:15 +0100

    Turn down, rather than off, ambient music.

--- Unity/Assets/Scripts/AmbientMusicControl.cs
+++ Unity/Assets/Scripts/AmbientMusicControl.cs
@@ -9,6 +9,12 @@ public class AmbientMusicControl : MonoBehaviour {
 	}
 
 	public eMusicOption Action;
+	
+	private const float VOLUME_CHANGE_DURATION = 1f;
+
+	private AudioSource _source;
+
+	private float _startTime;
 
 	void Start ()
 	{
@@ -16,22 +22,32 @@ public class AmbientMusicControl : MonoBehaviour {
 
 		if (ambientAudio != null)
 		{
-			AudioSource source = ambientAudio.GetComponent<AudioSource> ();
-			switch (Action)
+			_startTime = Time.time;
+			_source = ambientAudio.GetComponent<AudioSource> ();
+		}
+	}
+
+	void Update()
+	{
+		if (_source == null)
+		{
+			return;
+		}
+
+		switch (Action)
+		{
+		case eMusicOption.On:
+			if (_source.volume < 1)
 			{
-			case eMusicOption.On:
-				if (!source.isPlaying)
-				{
-					source.Play ();
-				}
-				break;
-			case eMusicOption.Off:
-				if (source.isPlaying)
-				{
-					source.Stop ();
-				}
-				break;
+				_source.volume = Mathf.Clamp01 ((Time.time - _startTime) / VOLUME_CHANGE_DURATION);
 			}
+			break;
+		case eMusicOption.Off:
+			if (_source.volume > 0)
+			{
+				_source.volume = 1 - Mathf.Clamp01 ((Time.time - _startTime) / VOLUME_CHANGE_DURATION);
+			}
+			break;
 		}
 	}
 }
