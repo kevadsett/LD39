89c1b4e85ecf9acfd8326964581885511469323b
Kev Adsett <kev.adsett@gmail.com>
2017-07-30 20:33:26 +0100

    Ambient sounds, placeholder game over text, piano death.

---
+++ Unity/Assets/Scripts/AmbientMusicControl.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class AmbientMusicControl : MonoBehaviour {
+	public enum eMusicOption
+	{
+		On,
+		Off
+	}
+
+	public eMusicOption Action;
+
+	void Start ()
+	{
+		GameObject ambientAudio = GameObject.Find ("AmbientAudio");
+
+		if (ambientAudio != null)
+		{
+			AudioSource source = ambientAudio.GetComponent<AudioSource> ();
+			switch (Action)
+			{
+			case eMusicOption.On:
+				if (!source.isPlaying)
+				{
+					source.Play ();
+				}
+				break;
+			case eMusicOption.Off:
+				if (source.isPlaying)
+				{
+					source.Stop ();
+				}
+				break;
+			}
+		}
+	}
+}
