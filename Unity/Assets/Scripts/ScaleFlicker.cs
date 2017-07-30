34684c08fdee981feddfaac89ec546f78586bfde
gav <notoriousknave@gmail.com>
2017-07-30 23:33:24 +0100

    made some things nice

---
+++ Unity/Assets/Scripts/ScaleFlicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleFlicker : MonoBehaviour {
+	[SerializeField] float amount;
+	[SerializeField] float freq;
+
+	float timer;
+
+	void Update () {
+		timer += Time.deltaTime;
+
+		if (timer > freq) {
+			transform.localScale = Vector3.one * (1f - Random.Range (0f, amount));
+		}
+	}
+}
