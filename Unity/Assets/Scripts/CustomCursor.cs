1a05b54dd54386cdd10b03704c58293703663879
Kev Adsett <kev.adsett@gmail.com>
2017-07-30 22:46:27 +0100

    Custom cursor.

---
+++ Unity/Assets/Scripts/CustomCursor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCursor : MonoBehaviour
+{
+	private RectTransform _rectTransform;
+	void Start ()
+	{
+		_rectTransform = GetComponent<RectTransform> ();
+	}
+
+	void Update ()
+	{
+		_rectTransform.position = Input.mousePosition;
+	}
+}
