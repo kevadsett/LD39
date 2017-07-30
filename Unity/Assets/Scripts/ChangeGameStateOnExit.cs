48ac833cc36b0347f5a8942d5ef94632bff7d0cf
Kev Adsett <kev.adsett@gmail.com>
2017-07-30 15:10:04 +0100

    Game States.

---
+++ Unity/Assets/Scripts/ChangeGameStateOnExit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGameStateOnExit : MonoBehaviour
+{
+	private GameStateMachine _gameStateMachine;
+
+	void Start ()
+	{
+		_gameStateMachine = GameObject.Find ("GameStateMachine").GetComponent<GameStateMachine> ();
+	}
+
+	public void WellDoneYouWon()
+	{
+		_gameStateMachine.ChangeState (GameStateMachine.eGameState.Win);
+	}
+}
---
+++ Unity/Assets/Scripts/PlayerExitCollisionDetection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerExitCollisionDetection : MonoBehaviour {
+	public float WaitTimeAfterExit = 1f;
+
+	private ChangeGameStateOnExit _exitHandler;
+
+	private bool _exitHandlerCalled;
+
+	void Start ()
+	{
+		_exitHandler = GameObject.Find ("ExitHandler").GetComponent<ChangeGameStateOnExit> ();
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log(collision);
+
+		if (LayerMask.LayerToName (collision.collider.gameObject.layer) != "Player")
+		{
+			return;
+		}
+
+		if (!_exitHandlerCalled)
+		{
+			StartCoroutine (DoExit ());
+		}
+	}
+
+	private IEnumerator DoExit ()
+	{
+		_exitHandlerCalled = true;
+		yield return new WaitForSeconds (WaitTimeAfterExit);
+		_exitHandler.WellDoneYouWon ();
+	}
+}
