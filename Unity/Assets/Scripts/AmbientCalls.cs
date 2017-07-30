7ed9a1dd58291be23f9f7ca6f15812838f9f3ef8
Kev Adsett <kev.adsett@gmail.com>
2017-07-30 20:10:54 +0100

    Ambient calls.

---
+++ Unity/Assets/Scripts/AmbientCalls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientCalls : MonoBehaviour {
+	public List<AudioClip> Sounds;
+
+	public float MaximumWait = 10f;
+
+	private AudioSource _audioSource;
+
+	private float _lastPlayTime;
+
+	void Start ()
+	{
+		_audioSource = GetComponent<AudioSource> ();
+		_lastPlayTime = Time.time;
+	}
+
+	void Update ()
+	{
+		if (Random.value > 0.999 && !_audioSource.isPlaying)
+		{
+			PlaySound ();
+		}
+		else
+		{
+			if (Time.time - _lastPlayTime >= MaximumWait)
+			{
+				PlaySound ();
+			}
+		}
+	}
+
+	private void PlaySound()
+	{
+		_audioSource.clip = Sounds [Random.Range (0, Sounds.Count)];
+		_audioSource.Play ();
+		_audioSource.panStereo = Random.Range (-1f, 1f);
+		_audioSource.volume = Random.Range (0.2f, 0.4f);
+		_audioSource.pitch = Random.Range (0.9f, 1.1f);
+		_lastPlayTime = Time.time;
+	}
+}
