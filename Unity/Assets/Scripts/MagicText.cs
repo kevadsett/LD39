a94913fbbfaab0282741f2fd42c76505be154d5e
gav <notoriousknave@gmail.com>
2017-07-30 21:25:08 +0100

    text exists

---
+++ Unity/Assets/Scripts/MagicText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicText : MonoBehaviour {
+	[SerializeField] Transform player;
+	[SerializeField] Color offColour;
+	[SerializeField] Color onColour;
+	[SerializeField] float distanceScale;
+	[SerializeField] Vector3 worldOffset;
+
+	TextMesh text;
+
+	void Awake () {
+		text = GetComponent<TextMesh> ();
+	}
+
+	void Update () {
+		float distance = Vector3.Distance (transform.position, player.position + worldOffset) * distanceScale;
+		text.color = Color.Lerp (onColour, offColour, distance);
+	}
+}
