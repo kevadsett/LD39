19a25687baca8fe541805ed2e6ec7fc16550456e
Kev Adsett <kev.adsett@gmail.com>
2017-07-30 13:06:41 +0100

    Basic scene management. Next up - actually put stuff in the scenes.

---
+++ Unity/Assets/Scripts/GameStateMachine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateMachine : MonoBehaviour {
+	public enum eGameState
+	{
+		Splash,
+		Main,
+		Lose,
+		Win
+	}
+
+	private Dictionary<eGameState, SceneLoader> _stateScenes;
+
+	private SceneLoader currentStateScene;
+
+	void Start ()
+	{
+		DontDestroyOnLoad (gameObject);
+		_stateScenes = new Dictionary<eGameState, SceneLoader>
+		{
+			{ eGameState.Splash, new SceneLoader("Splash") },
+			{ eGameState.Main, new SceneLoader("main") },
+			{ eGameState.Win, new SceneLoader("Win") },
+			{ eGameState.Lose, new SceneLoader("Lose") },
+		};
+
+		ChangeState (eGameState.Splash);
+	}
+
+	public void ChangeState(eGameState newState)
+	{
+		if (currentStateScene != null)
+		{
+			currentStateScene.OnExit ();
+		}
+
+		currentStateScene = _stateScenes [newState];
+		currentStateScene.OnEnter ();
+	}
+}
---
+++ Unity/Assets/Scripts/GameStates/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine.SceneManagement;
+
+public class SceneLoader
+{
+	private string _scene;
+
+	public SceneLoader (string sceneName)
+	{
+		_scene = sceneName;
+	}
+
+	public void OnEnter ()
+	{
+		SceneManager.LoadScene (_scene);
+	}
+
+	public void OnExit ()
+	{
+		SceneManager.UnloadSceneAsync (_scene);
+	}
+}
