0fcb651d84cf3b4bdb659d3e695604e0e4dd25ec
gav <notoriousknave@gmail.com>
2017-07-31 21:21:27 +0100

    ending is now a slideshow

---
+++ Unity/Assets/Scripts/TextClickThru.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof (Text))]
+public class TextClickThru : MonoBehaviour {
+	[SerializeField] string[] texts;
+	[SerializeField] bool exitOnFinished;
+
+	Text uiText;
+	int textIndex;
+
+	void Awake () {
+		uiText = GetComponent<Text> ();
+	}
+
+	void Update () {
+		if (textIndex < texts.Length) {
+			uiText.text = texts[textIndex];
+		} else {
+			uiText.text = texts[texts.Length - 1];
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+			textIndex++;
+		}
+
+		if (exitOnFinished && textIndex > texts.Length) {
+			Application.Quit ();
+		}
+	}
+}
